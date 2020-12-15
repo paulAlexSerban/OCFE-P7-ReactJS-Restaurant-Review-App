79c39d6bc48d9bb930b9368edf88243ee3dc3e9e
Paul Serban <paul.serban@netcentric.biz>
2020-12-15 16:08:45 +0200

    OCFE-p7-05-UPDATED-fixed-code-consistency-issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+// Import React dependencies
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Import root SCSS file
 import "./index.scss";
+
+// Import root basepage
 import Basepage from "./components/templates/basepage/Basepage";
+
+// Import 3rd party libraries
 import "primereact/resources/themes/nova-light/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
