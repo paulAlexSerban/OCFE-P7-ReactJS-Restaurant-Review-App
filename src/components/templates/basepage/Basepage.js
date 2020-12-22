946ee2d7cb9662f75c6bc1ca9483ab476fa3bb1b
Paul Serban <paul.serban@netcentric.biz>
2020-12-22 03:49:54 +0200

    OCFE-p7-08-UPDATED-basepage.js

--- src/components/templates/basepage/Basepage.js
+++ src/components/templates/basepage/Basepage.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import Main from '../../structure/main/Main';
-import Header from '../../structure/header/Header';
-import Footer from '../../structure/footer/Footer';
+import React from "react";
+import Main from "../../structure/main/Main";
+import Header from "../../structure/header/Header";
+import Footer from "../../structure/footer/Footer";
 
-export class Basepage extends Component {
-  render() {
-    return (
-      <div className="basepage">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    )
-  }
-}
+const basepage = () => {
+  return (
+    <div className="basepage">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
 
-export default Basepage
+export default basepage;
